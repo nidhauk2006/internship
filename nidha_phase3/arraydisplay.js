18d88b262d179bf58e20a4bb79b15d95738621b0
nidhauk.2006@gmail.com <nidhauk.2006@gmail.com>
2026-04-29 21:03:21 +0530

    Removed old files and added new HTML tasks

--- nidha_phase3/arraydisplay.js
+++
@@ -1,9 +0,0 @@
-let items = ["Pen", "Book", "Bag", "Bottle"];
-
-let list = document.getElementById("list");
-
-items.forEach(function(item) {
-  let li = document.createElement("li");
-  li.innerText = item;
-  list.appendChild(li);
-});
--- nidha_phase3/evenodd.js
+++
@@ -1,9 +0,0 @@
-function check() {
-  let num = document.getElementById("num").value;
-
-  if (num % 2 === 0) {
-    document.getElementById("result").innerText = "Even";
-  } else {
-    document.getElementById("result").innerText = "Odd";
-  }
-}
